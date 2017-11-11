68fccdae6825a8fbbeb243bf9c64fe77a7607c81
Matthew <wattmills@gmail.com>
2017-11-11 12:12:17 -0500

    get bootstrap from CDN rather than local storage

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php require "php/serverConnect.php"; ?>
 <html lang="en" ng-app="kitchensink">
 <head>
-<title>HDS Custom Products</title>
+<title>Canvas Demo</title>
 <link rel="icon" href="assets/icon.png" type="image/x-icon">
 
-<link href="css/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"><link href="css/bootstrap.css" rel="stylesheet">
 <link href="css/kitchensink.css" rel="stylesheet">
-<link href="css/hds.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
 
 <script src="js/jquery.js"></script>
-<script src="js/bootstrap.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script src="js/angular.min.js"></script>
 <script src="js/fabric.js"></script>
 
--- register.php
+++ register.php
@@ -4,11 +4,11 @@
 <title>Create a New Account</title>
 <link rel="icon" href="assets/icon.png" type="image/x-icon">
 
-<link href="css/bootstrap.css" rel="stylesheet">
-<link href="css/hds.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"><link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
 
 <script src="js/jquery.js"></script>
-<script src="js/bootstrap.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
   <nav class="navbar navbar-default" role="navigation">
